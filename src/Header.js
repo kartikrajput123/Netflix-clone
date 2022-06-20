682123db1589555f2815cfee7f46ce3cea8662f4
kartikrajput123 <44284328+kartikrajput123@users.noreply.github.com>
2022-06-20 12:45:40 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -9,13 +9,6 @@ function Header() {
             className="netflix__logo"
              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt="NETFLIX" />
-            <div className="header__links">
-                <button>Home</button>
-                <button>TV Shows</button>
-                <button>Movies</button>
-                <button>New and Popular</button>
-                <button>My List</button>
-            </div>
             <a href='/'>
             <img
             className="user__avatar"
